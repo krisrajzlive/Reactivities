f447591b68fabcabe30283189529ee9fc4557355
krisrajz <krisrajz@gmail.com>
2020-08-11 14:51:28 +0530

    Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,13 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             }
             );
+            services.AddCors(
+            opt=> {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");        
+                });
+            });
             services.AddControllers();
         }
 
@@ -45,7 +52,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
-
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
