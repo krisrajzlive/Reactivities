de6bf8a08f2505913fe9916dbe61d199981ab405
krisrajz <krisrajz@gmail.com>
2020-10-12 22:57:59 +0530

    End of Session 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
-//import 'react-toastify/dist/ReactToastify.min.css';
-//import 'react-widgets/dist/css/react-widgets.css';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 
